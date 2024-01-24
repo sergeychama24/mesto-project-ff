d42877627d785066cd8277a770f173bebdd94f8b
sergeychama24 <sergeychama24@gmail.com>
2024-01-24 08:46:21 +0300

    0.0.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,17 @@
 // @todo: Темплейт карточки
 
+const cardList = document.querySelector('.places__list')
+const cardTemplate = document.querySelector('#card-template').content;
+const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+cardList.append(cardElement);
+
 // @todo: DOM узлы
+
 
 // @todo: Функция создания карточки
 
+
 // @todo: Функция удаления карточки
+
 
 // @todo: Вывести карточки на страницу
