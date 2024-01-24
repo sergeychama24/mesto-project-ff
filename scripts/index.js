f7f8a5317c4d69dff286bd5e28e64663de5c1394
sergeychama24 <sergeychama24@gmail.com>
2024-01-24 16:46:50 +0300

    1.1.0

--- scripts/index.js
+++ scripts/index.js
@@ -3,12 +3,21 @@ const cardList = document.querySelector('.places__list');
 function createCard(initialCards, deleteCard) {
     initialCards.forEach(function (element) {
         const cardElement= cardTemplate.querySelector('.places__item').cloneNode(true);
-        cardElement.querySelector('.card__title').textContent = element.name;
-        cardElement.querySelector('.card__image').src = element.link;
-        cardElement.querySelector('.card__image').alt = element.name;
-        cardElement.querySelector('.card__delete-button').addEventListener('click', function() {
+
+        const cardTitle = cardElement.querySelector('.card__title')
+        cardTitle.textContent = element.name;
+
+        const cardImageSource = cardElement.querySelector('.card__image');
+        cardImageSource.src = element.link;
+
+        const cardImageAlt = cardElement.querySelector('.card__image')
+        cardImageAlt.alt = element.name;
+
+        const cardDeleteButton = cardElement.querySelector('.card__delete-button')
+        cardDeleteButton.addEventListener('click', function() {
           deleteCard(cardElement)
         } );
+
         cardList.append(cardElement);
     })
 }
