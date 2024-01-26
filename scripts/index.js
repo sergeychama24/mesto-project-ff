33065e2222fb7e94eae3fb0c96810f78262d8aaf
sergeychama24 <sergeychama24@gmail.com>
2024-01-26 08:40:56 +0300

    3.0.0

--- scripts/index.js
+++ scripts/index.js
@@ -2,32 +2,36 @@ const cardTemplate = document.querySelector('#card-template').content;
 const cardList = document.querySelector('.places__list');
 
 function createCard(cardTemplate, deleteCard) {
-        const cardElement= cardTemplate.querySelector('.places__item').cloneNode(true);
+    const cardElement= cardTemplate.querySelector('.places__item').cloneNode(true);
 
-        const cardDeleteButton = cardElement.querySelector('.card__delete-button');
-        cardDeleteButton.addEventListener('click', function() {
+    const cardImage = cardElement.querySelector('.card__image');
+    cardImage.src = cardElement.link;
+    cardImage.alt = cardElement.name;
+
+    const cardTitle = cardElement.querySelector('.card__title')
+    cardTitle.textContent = cardElement.name;
+
+    const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+    cardDeleteButton.addEventListener('click', function() {
           deleteCard(cardElement)
         });
-        cardDeleteButton.removeEventListener('click', function() {
-            deleteCard(cardElement)
-        })
 
-        return cardElement
+    return cardElement
 }
 
 function deleteCard(cardElement) {
     cardElement.remove()
 }
 
-initialCards.forEach((element) =>{
+initialCards.forEach((element) => {
     const card = createCard(cardTemplate, deleteCard);
+
+    card.querySelector('.card__title').textContent = element.name;
 
     const cardImage = card.querySelector('.card__image');
     cardImage.src = element.link;
     cardImage.alt = element.name;
 
-    const cardTitle = card.querySelector('.card__title')
-    cardTitle.textContent = element.name;
-
     cardList.append(card);
 })
+
