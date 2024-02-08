e864849b6cc6618d63b3e631aac54f5fa0dee46c
sergeychama24 <sergeychama24@gmail.com>
2024-02-08 08:32:09 +0300

    4.5.1

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import './pages/index.css'
 import {initialCards} from "./components/cards";
 import {cardList, deleteCard, createCard} from "./components/card";
 
-
 initialCards.forEach((dataCard) => {
     const card = createCard(dataCard, deleteCard);
-
     cardList.append(card);
 })
